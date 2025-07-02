2617371870663e080a68eff89f9a7cc22cdb63a7
MUWONGE HASSAN <hassansaava@gmail.com>
2025-07-02 16:17:15 +0300

    update

---
+++ Infrastructure/Identity/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //public string FullName => $"{FirstName} {LastName}";
+        //public string? TenantId { get; set; } // Nullable to allow for non-tenant users
+        public string RefreshToken { get; set; } // Default to empty string
+        public DateTime RefreshTokenExpiryTime { get; set; } // Default to 30 days from now
+        public bool IsActive { get; set; } // Default to active
+    }
+}
---
+++ Infrastructure/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Finbuckle.MultiTenant;
+using Infrastructure.Contexts;
+using Infrastructure.Tenancy;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class Startup
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add your infrastructure services here
+            // For example, database context, repositories, etc.
+
+            // Example:
+            return services.AddDbContext<TenantDbContext>(options =>
+                  options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")))
+                 .AddMultiTenant<ABCSchoolTenantInfo>()
+                 .WithHeaderStrategy(TenancyConstants.TenantIdName)
+                 .WithClaimStrategy(TenancyConstants.TenantIdName)
+                 .WithEFCoreStore<TenantDbContext, ABCSchoolTenantInfo>().Services
+                 .AddDbContext<ApplicationDbContext>(options =>
+                     options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        }
+
+        public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+        {
+            // Configure your middleware here
+            // For example, authentication, logging, etc.
+            // Example:
+            app.UseMultiTenant();
+            return app;
+        }
+    }
+}
